d3f7335f3844d80e79be04f77e9a6efa2475f2e9
Messias Cardoso <messiascardoso@gmail.com>
2020-07-29 13:36:34 -0300

    Add router and comp. slider

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import Logo from '../../assets/LogoMain.png';
 import './Menu.css';
-//import ButtonLink from '../ButtonLink';
+//import ButtonLink from '../ButtonLink'Link
 import Button from '../Button';
 
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            {/* Link faz funcionar o SPA */}
+            <Link to="/">
                 <img className="Logo" src={ Logo } alt ="Meflix" ></img>
-            </a>
-            <Button as="a" className="ButtonLink" href="/">
+            </Link>
+            <Button as={ Link } className="ButtonLink" to="/cadastro/video">
                   Novo vídeo  
             </Button>
         </nav>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/home';
 // import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import CadastroVideo from './pages/Video';
+import CadastroCategoria from './pages/cadastro';
+
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // Add routes com React-router-dom
+  <BrowserRouter>
+    <Switch>
+      {/* Qaundo add exact, a rota tem que ser exata */}
+      <Route path="/" component={Home} exact/> 
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      {/* Remover o path para quando não achar nenhuma rota */}
+      <Route component={ ()=> (<div>Pagina 404</div>)} />  
+    </Switch>
+  </BrowserRouter>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 
