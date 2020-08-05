5e3932f685cceee26e7bdfdc4c89881c60b6ed2f
Messias Cardoso <messiascardoso@gmail.com>
2020-08-04 22:13:21 -0300

    Add JsLint and json server

--- src/index.js
+++ src/index.js
@@ -1,34 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/home';
 // import * as serviceWorker from './serviceWorker';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import CadastroVideo from './pages/Video';
 import CadastroCategoria from './pages/cadastro';
-
-
-
 
 ReactDOM.render(
   // Add routes com React-router-dom
   <BrowserRouter>
     <Switch>
       {/* Qaundo add exact, a rota tem que ser exata */}
-      <Route path="/" component={Home} exact/> 
+      <Route path="/" component={Home} exact />
       <Route path="/cadastro/video" component={CadastroVideo} />
       <Route path="/cadastro/categoria" component={CadastroCategoria} />
       {/* Remover o path para quando não achar nenhuma rota */}
-      <Route component={ ()=> (<div>Pagina 404</div>)} />  
+      <Route component={() => (<div>Pagina 404</div>)} />
     </Switch>
   </BrowserRouter>,
   // <React.StrictMode>
   //   <App />
   // </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-//serviceWorker.unregister();
+// serviceWorker.unregister();
